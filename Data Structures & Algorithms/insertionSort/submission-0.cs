ed3315eada02f712289354804aa55d9c91e66d99
Jacob Snyder <95860834+jsGH314@users.noreply.github.com>
2026-05-06 12:32:56 -0500

    Add: insertionSort - submission-0

---
+++ Data Structures & Algorithms/insertionSort/submission-0.cs
@@ -0,0 +1,32 @@
+// Definition for a pair
+// public class Pair {
+//     public int Key;
+//     public string Value;
+//
+//     public Pair(int key, string value) {
+//         Key = key;
+//         Value = value;
+//     }
+// }
+
+public class Solution 
+{
+    public List<List<Pair>> InsertionSort(List<Pair> pairs) 
+    {
+        List<List<Pair>> result = new List<List<Pair>>();
+
+        for(int i = 0; i < pairs.Count; i++)
+        { 
+            int j = i - 1;
+            while(j >= 0 && pairs[j + 1].Key < pairs[j].Key)
+            { 
+                Pair temp = pairs[j + 1];
+                pairs[j + 1] = pairs[j];
+                pairs[j] = temp;
+                j -= 1;
+            }
+            result.Add(new List<Pair>(pairs));
+        }
+        return result;
+    }
+}
